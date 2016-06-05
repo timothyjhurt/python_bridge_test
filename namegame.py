4ce7b0d6a656dc36b0e4962003f4e30ee321926a
timothyjhurt <timothyjhurt@gmail.com>
2016-06-05 12:21:25 -0700

    Making it work

--- namegame.py
+++ namegame.py
@@ -6,7 +6,7 @@ else:
     name = input("Enter your name: ")
 
 for i in range(len(name), 0, -1):
-    print( name[0: i], end " " )
+    print( name[0: i], end = " " )
     for j in range(i, len(name)):
         print(" " * (j-i) + name[j], end = "")
     print("")
